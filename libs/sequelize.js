b8698d4740c0752582e579acec6a743ba9c4c185
David H <davyd2h@gmail.com>
2025-03-25 14:11:34 -0500

    configuracion Sequelize

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,4 +1,14 @@
 const { Sequelize } = require('sequelize');
-const { config } = require('./config');
-const setupModels = require('../bd/models');
+const { config } = require('../config/config');
+//const setupModels = require('../bd/models');
 
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.name}`;
+
+const sequelize = new Sequelize(URI, {
+    dialect: 'postgres',
+    logging: true,
+});
+
+module.exports = sequelize
