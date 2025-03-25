3ec94daa07fabfd6c896602a586747c096a91210
David H <davyd2h@gmail.com>
2025-03-25 07:55:09 -0500

    configuracion de mi DB, se crea la estructura base de la aplicación, con el principio de responsabilidad

---
+++ bd/models/index.js
@@ -0,0 +1,8 @@
+const { Model } = require('sequelize');
+const {User, UserSchema } = require('./user.model');
+
+function setupModels(sequelize){
+    User.init(UserSchema, User.config(sequelize))
+}
+
+module.exports = setupModels;
---
+++ libs/sequelize.js
@@ -0,0 +1,4 @@
+const { Sequelize } = require('sequelize');
+const { config } = require('./config');
+const setupModels = require('../bd/models');
+
