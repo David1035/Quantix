cab99d1852d601122df53c110368837268eb216b
David H <davyd2h@gmail.com>
2025-03-28 10:45:56 -0500

    modelo de client, pendiente las relaciones futuras

---
+++ bd/models/client.model.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const CLIENT_TABLE = 'client';
+
+const clientSchema = {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primarykey: true,
+        autoIncrement: true
+    }, 
+    document: {
+        type: DataTypes.NUMBER,
+        allowNull: false,
+        unique: true
+    },
+    name: {
+        type: DataTypes.VARCHAR(70),
+        allowNull: false
+
+    },
+    phone: {
+        type: DataTypes.VARCHAR(20),
+        allowNull: false
+    }, 
+    estado_credito: {
+        type: DataTypes.VARCHAR(20),
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW
+    }
+}
+
+class client extends Model {
+    static associate(models){
+        // relaciones futuras
+    }
+
+    static config(sequelize){
+        return {
+            sequelize, 
+            tableName: CLIENT_TABLE,
+            modelName: 'client',
+            timestamps: false
+        }
+    }
+}
+
+
+module.exports = { CLIENT_TABLE, clientSchema, client }  // exportar la clase y el schema
