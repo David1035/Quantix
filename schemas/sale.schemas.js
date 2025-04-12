2187fc68511e7a5626d02e3f468e2e0f53c10252
David Hernandez! <davyd2h@gmail.com>
2025-04-12 18:21:35 -0500

    se crea schema de venta

---
+++ schemas/sale.schemas.js
@@ -0,0 +1,27 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer().positive();
+const fecha_venta = Joi.date();
+const total_venta = Joi.number();
+const fk_id_cliente = Joi.number().integer().positive();
+const fk_id_usuario = Joi.number().integer().positive();
+
+const createSaleSchema = Joi.object({
+    fecha_venta: fecha_venta.required(),
+    total_venta: total_venta.required(),
+    fk_id_cliente: fk_id_cliente.required(),
+    fk_id_usuario: fk_id_usuario.required()
+})
+
+const updateSaleSchema = Joi.object({
+    fecha_venta: fecha_venta,
+    total_venta: total_venta,
+    fk_id_cliente: fk_id_cliente,
+    fk_id_usuario: fk_id_usuario
+}).min(1);
+
+const deleteSaleSchema = Joi.object({ 
+    id: id.required()
+});
+
+module.exports = { createSaleSchema, updateSaleSchema, deleteSaleSchema }; 
